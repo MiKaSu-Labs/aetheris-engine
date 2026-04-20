4bee0c8e5875f14f5a34183b3b8a6415acd00ae2
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-04-20 21:41:28 +0900

    chore(debug_constants): update header metadata and build instructions in source banner

--- src/main/server/aetheris/debug_constants.c
+++ src/main/server/aetheris/debug_constants.c
@@ -1,5 +1,27 @@
 /*
- * debug_constants.c
+ * 
+ *           ______ _______ _    _ ______ _____  _____  _____ 
+ *     /\   |  ____|__   __| |  | |  ____|  __ \|_   _|/ ____|
+ *    /  \  | |__     | |  | |__| | |__  | |__) | | | | (___  
+ *   / /\ \ |  __|    | |  |  __  |  __| |  _  /  | |  \___ \ 
+ *  / ____ \| |____   | |  | |  | | |____| | \ \ _| |_ ____) |
+ * /_/    \_\______|  |_|  |_|  |_|______|_|  \_\_____|_____/ 
+ * 
+ *                                                                                     v1.0
+ * @name Aetheris
+ *
+ * @author dkitagawa
+ *
+ * @file debug_constants.c
+ *
+ * @brief Defines runtime debug flags and security-sensitive debug options.
+ *
+ * Implements global debug toggles used for logging abilities, Lua scripts,
+ * quests, and missing resource diagnostics. Also defines a critical security
+ * flag controlling whether client-provided authentication tokens are accepted.
+ *
+ * @copyright (c) 2026 Douglas Kitagawa
+ *
  */
 
 #include "debug_constants.h"
