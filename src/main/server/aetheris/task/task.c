eee15cde0b42707a3e65f128ce250bae2d73a72b
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-05-20 16:26:01 +0900

    docs(tasks): add header comments and file metadata for task scheduler

---
+++ src/main/server/aetheris/task/task.c
@@ -0,0 +1,26 @@
+/*
+ * 
+ *           ______ _______ _    _ ______ _____  _____  _____ 
+ *     /\   |  ____|__   __| |  | |  ____|  __ \|_   _|/ ____|
+ *    /  \  | |__     | |  | |__| | |__  | |__) | | | | (___  
+ *   / /\ \ |  __|    | |  |  __  |  __| |  _  /  | |  \___ \ 
+ *  / ____ \| |____   | |  | |  | | |____| | \ \ _| |_ ____) |
+ * /_/    \_\______|  |_|  |_|  |_|______|_|  \_\_____|_____/ 
+ * 
+ *                                                                                     v1.0
+ * @name Aetheris
+ *
+ * @author dkitagawa
+ *
+ * @file task.c
+ *
+ * @brief Task scheduler annotation descriptor implementation.
+ *        The ae_task_def_t type is a pure data descriptor with no
+ *        shared logic. Scheduling behaviour is implemented in
+ *        task_manager.c.
+ *
+ * @copyright (c) 2026 Douglas Kitagawa
+ *
+ */
+
+#include "task.h"
