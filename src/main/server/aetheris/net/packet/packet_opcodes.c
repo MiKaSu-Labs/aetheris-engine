d094aba49c27b016195b419286a5cdd0c426335a
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-06 18:27:02 +0900

    docs(network): add implementation unit and documentation for packet opcode definitions

---
+++ src/main/server/aetheris/net/packet/packet_opcodes.c
@@ -0,0 +1,26 @@
+/*
+ * 
+ *           ______ _______ _    _ ______ _____  _____  _____ 
+ *     /\   |  ____|__   __| |  | |  ____|  __ \|_   _|/ ____|
+ *    /  \  | |__     | |  | |__| | |__  | |__) | | | | (___  
+ *   / /\ \ |  __|    | |  |  __  |  __| |  _  /  | |  \___ \ 
+ *  / ____ \| |____   | |  | |  | | |____| | \ \ _| |_ ____) |
+ * /_/    \_\______|  |_|  |_|  |_|______|_|  \_\_____|_____/ 
+ * 
+ *                                                                                     v1.0
+ * @name Aetheris
+ *
+ * @author dkitagawa
+ *
+ * @file packet_opcodes.c
+ *
+ * @brief Packet opcode table implementation.
+ *        All opcode constants are compile-time #define macros with no
+ *        shared logic. Packet dispatch behaviour based on these values
+ *        is implemented in packet_handler.c and the packet router.
+ *
+ * @copyright (c) 2026 Douglas Kitagawa
+ *
+ */
+
+#include "packet_opcodes.h"
